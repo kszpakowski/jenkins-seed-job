2a4d58badb28f57124daa9ea43cda382cbe844d8
kszpakowski <karol.szpakowski@gmail.com>
2021-11-11 20:25:05 +0100

    add job script

--- src/jobs/verifyJiraTicket.groovy
+++ src/jobs/verifyJiraTicket.groovy
@@ -3,7 +3,26 @@ import scripts.Config
 Config.process(config, {}, { projectConf ->
     if(projectConf.expectedIssueStatus) {
         pipelineJob("${projectConf.fullName()}/verifyJiraTicket"){
-            
+            definition {
+                cps {
+                    script('''
+                    pipeline {
+                        agent any
+
+                        stages {
+                            stage('Verify Jira Ticket') {
+                                steps {
+                                    script {
+                                        echo "Checking jira ticket. Jira project: ${projectConf.jiraProject}"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    ''')
+                    sandbox()
+                }
+            }
         }
     }
 })
