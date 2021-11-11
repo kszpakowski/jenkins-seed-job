0cf9ff2b6f300917e035943aadb23c4b83de57b0
kszpakowski <karol.szpakowski@gmail.com>
2021-11-11 21:03:04 +0100

    add verify steps

--- src/jobs/verify.groovy
+++ src/jobs/verify.groovy
@@ -2,6 +2,34 @@ import scripts.Config
 
 Config.process(config, {}, { projectConf ->
     pipelineJob("${projectConf.fullName()}/verify"){
-        
+        definition {
+            cps {
+                script("""
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout) {
+            steps {
+                script {
+                    echo "Checking out.."
+                    
+                }
+            }
+        }
+        stage('Test and Analysis') {
+            parallel 'Unit Tests': {
+                echo "Running unit tests.."
+            }, 'Static Analysis': { 
+                echo "Running Static Analysis"
+            }
+
+        }
+    }
+}
+                """)
+                sandbox()
+            }
+        }
     }
 })
