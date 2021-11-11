9a48fa6d87fadaf5e58146cdee67668b68169a9d
kszpakowski <karol.szpakowski@gmail.com>
2021-11-11 16:47:47 +0100

    add wrapper method

--- src/jobs/scripts/Config.groovy
+++ src/jobs/scripts/Config.groovy
@@ -2,13 +2,17 @@ package scripts
 
 class Config{
 
-    public void process(def config, def parentFolder='', def Closure folderCallback, def Closure projectCallback){
+    public static void process(def config, def Closure folderCallback, def Closure projectCallback){
+        processConfig(config, folderCallback, projectCallback)
+    }
+
+    private void processConfig(def config, def parentFolder='', def Closure folderCallback, def Closure projectCallback){
         config.folders.each() {
             def fullName = "${parent}${it.name}"
             def folder = new Folder(fullName, it.securityGroups)
             folderCallback(folder)
             if(it.folders){
-                process(it, "${fullName}/", folderCallback, projectCallback)
+                processConfig(it, "${fullName}/", folderCallback, projectCallback)
             }
             if(it.projects){
                 it.projects.each {
