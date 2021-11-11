175927010f99b7ffae1b1a9069a97058793589b3
kszpakowski <karol.szpakowski@gmail.com>
2021-11-11 19:53:04 +0100

    add stage to verify job

--- src/jobs/verify.groovy
+++ src/jobs/verify.groovy
@@ -2,6 +2,14 @@ import scripts.Config
 
 Config.process(config, {}, { projectConf ->
     pipelineJob("${projectConf.fullName()}/verify"){
-        
+        stages {
+            stage('Checkout'){
+                steps {
+                    script {
+                        echo "Checking out"
+                    }
+                }
+            }
+        }
     }
 })
