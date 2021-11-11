58c5f0d19f60b810887a41af354976d0a7f030b8
kszpakowski <karol.szpakowski@gmail.com>
2021-11-11 23:19:06 +0100

    add job parameters

--- src/jobs/performanceTest.groovy
+++ src/jobs/performanceTest.groovy
@@ -3,6 +3,9 @@ import scripts.Config
 Config.process(config, {}, { projectConf ->
     if(projectConf.generateTestAutomationJobs) {
         pipelineJob("${projectConf.fullName()}/performanceTest"){
+            parameters {
+                choiceParam('Environment',['Dev','Test'])
+            }
             definition {
                 cps {
                     script("""
